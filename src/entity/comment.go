ae96996133e1fe1c044a3435c0e13968bf317f9f
Michael Shindarev <mshindarev@gmail.com>
2025-02-25 23:28:50 +0300

    remained to debag

--- src/entity/comment.go
+++ src/entity/comment.go
@@ -3,13 +3,13 @@ package entity
 import "time"
 
 type Comment struct {
-	ID              int        `json:"id"`
-	UserId          int        `json:"userId"`
-	PostId          int        `json:"postId"`
-	Body            string     `json:"body"`
-	ParentId        *int       `json:"parent"`
-	CreationTime    time.Time  `json:"creationTime"`
-	RelatedComments []*Comment `json:"relatedComments"`
+	ID              int        `json:"id" db:"id"`
+	UserId          int        `json:"userId" db:"user_id"`
+	PostId          int        `json:"postId" db:"post_id"`
+	Body            string     `json:"body" db:"body"`
+	ParentId        *int       `json:"parent" db:"parent"`
+	CreationTime    time.Time  `json:"creationTime" db:"creation_time"`
+	RelatedComments []*Comment `json:"relatedComments" db:"related_comments"`
 }
 
 func (c *Comment) GetCreationTime() time.Time {
